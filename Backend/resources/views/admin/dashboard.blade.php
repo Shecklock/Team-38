18bd74f2e57a8eac895d05c21c9f277588bff22f
MattBrian22 <briantahirmatthew@gmail.com>
2023-11-10 13:26:51 +0000

    Commit files required for back-end

---
+++ Backend/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.inc/admin')
+
+@section('content')
+
+    hello
+    
+@endsection
+
---
+++ Backend/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider, and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Correcting the route group
+Route::prefix('admin')->group(function () {
+    // You can define more routes specific to the 'dashboard' prefix here
+    Route::get('dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard.index');
+});
