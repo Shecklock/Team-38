ed764645767072dca37855536a8844068546ecec
ZA3N <220180979@aston.ac.uk>
2024-03-14 14:32:28 +0000

    Made Visual Changes
    Made some visual changes to header and footer

--- Backend/resources/views/viewproduct.blade.php
+++ Backend/resources/views/viewproduct.blade.php
@@ -17,118 +17,118 @@
 
 
 <body>
-    <div class="py-3 py-md-5 bg-light">
-        <div class="container">
-            <div class="row">
-                <!-- Product Image Column -->
-                <div class="col-md-4 mt-3">
-                    <div class="bg-white border">
-                        <img src="{{ asset('uploads/product/' . $product->image) }}" alt="" class="productImg" style="width: 100%; height: auto;">
-                    </div>
-                </div>
-
-                <!-- Product Purchasing Details Column -->
-                <div class="col-md-4 mt-3">
-                    <div class="product-view">
-                        <h1 class="productTitle">{{ $product->ProductName }}
-                            <label class="label-stock bg-success">In Stock</label>
-                        </h1>
-                        <hr>
-                        <p class="product-path">
-                            <a href="{{ url('/search?search=Men') }}" class="path-link">Men</a> /
-                            <a href="{{ url('/search?search=Women') }}" class="path-link">Women</a> /
-                            <a href="{{ url('/search?search=Trainers') }}" class="path-link">Trainers</a> /
-                            <a href="{{ url('/search?search=Accessories') }}" class="path-link">Accessories</a>
-                        </p>
-                        <div>
-                            <span class="productPrice">£{{ $product->Price }}</span>
-                        </div>
-                        <div class="mt-2">
-                            <div class="input-group">
-                                <span class="btn btn1"><i class="fa fa-minus"></i></span>
-                                <input type="text" value="1" class="input-quantity" />
-                                <span class="btn btn1"><i class="fa fa-plus"></i></span>
-                            </div>
-                        </div>
-                        <div class="mt-2">
-                            <a href="{{ route('add-to-basket', ['productId' => $product->ProductID]) }}" class="btn btn1"> <i class="fa fa-shopping-cart"></i> Add To Cart</a>
-                        </div>
-                        <div class="mt-3">
-                            <p class="productDesc">{{ $product->Description }}</p>
+        <div class="py-3 py-md-5 bg-light">
+            <div class="container">
+                <div class="row">
+                    <!-- Product Image Column -->
+                    <div class="col-md-4 mt-3">
+                        <div class="bg-white border">
+                            <img src="{{ asset('uploads/product/' . $product->image) }}" alt="" class="productImg" style="width: 100%; height: auto;">
                         </div>
                     </div>
-                </div>
 
-                <!-- Create a Review Column -->
-                <div class="col-md-4 mt-3">
-                    <div class="reviews">
-                        <h2>Write a Review</h2>
-                        <form id="review-form" action="{{ route('reviews.store', ['product_id' => $product->ProductID]) }}" method="POST">
-                            @csrf
-                            <div class="mb-3">
-                                <label for="reviewer-name" class="form-label">Your Name</label>
-                                <input type="text" class="form-control" name="reviewer-name" required>
+                    <!-- Product Purchasing Details Column -->
+                    <div class="col-md-4 mt-3">
+                        <div class="product-view">
+                            <h1 class="productTitle">{{ $product->ProductName }}
+                                <label class="label-stock bg-success">In Stock</label>
+                            </h1>
+                            <hr>
+                            <p class="product-path">
+                                <a href="{{ url('/search?search=Men') }}" class="path-link">Men</a> /
+                                <a href="{{ url('/search?search=Women') }}" class="path-link">Women</a> /
+                                <a href="{{ url('/search?search=Trainers') }}" class="path-link">Trainers</a> /
+                                <a href="{{ url('/search?search=Accessories') }}" class="path-link">Accessories</a>
+                            </p>
+                            <div>
+                                <span class="productPrice">£{{ $product->Price }}</span>
                             </div>
-                            <div class="mb-3">
-                                <label for="review-text" class="form-label">Your Review</label>
-                                <textarea class="form-control" name="review-text" rows="3" required></textarea>
+                            <div class="mt-2">
+                                <div class="input-group">
+                                    <span class="btn btn1"><i class="fa fa-minus"></i></span>
+                                    <input type="text" value="1" class="input-quantity" />
+                                    <span class="btn btn1"><i class="fa fa-plus"></i></span>
+                                </div>
                             </div>
-                            <div class="star-rating mb-3">
-                                @for ($i = 1; $i <= 5; $i++)
-                                    <span class="star" data-value="{{ $i }}">&#9733;</span>
-                                @endfor
-                                <input type="hidden" name="rating" id="rating" value="">
+                            <div class="mt-2">
+                                <a href="{{ route('add-to-basket', ['productId' => $product->ProductID]) }}" class="btn btn1"> <i class="fa fa-shopping-cart"></i> Add To Cart</a>
                             </div>
-                            <button type="submit" class="btn btn-primary">Submit Review</button>
-                        </form>
+                            <div class="mt-3">
+                                <p class="productDesc">{{ $product->Description }}</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
 
-            <!-- Previous Reviews Row -->
-            <div class="row mt-3">
-    <div class="col-12">
-        <div class="reviews">
-            <div class="CustomerRev">
-                <hr>
-                <h3>Customer Reviews</h3>
-                <hr>
-            </div>
-            <div class="row">
-                @forelse ($product->reviews()->latest()->take(3)->get() as $review)
-                    <div class="col-md-4">
-                        <div class="review">
-                            <h4>{{ $review->reviewer_name }}</h4>
-                            <div class="review-text">
-                                <p>{{ $review->review_text }}</p>
-                            </div>
-                            <div class="previous-star-rating">
-                                @for ($i = 1; $i <= 5; $i++)
-                                    @if ($i <= $review->rating)
-                                        <span class="star filled" data-value="{{ $i }}">&#9733;</span>
-                                    @else
+                    <!-- Create a Review Column -->
+                    <div class="col-md-4 mt-3">
+                        <div class="reviews">
+                            <h2>Write a Review</h2>
+                            <form id="review-form" action="{{ route('reviews.store', ['product_id' => $product->ProductID]) }}" method="POST">
+                                @csrf
+                                <div class="mb-3">
+                                    <label for="reviewer-name" class="form-label">Your Name</label>
+                                    <input type="text" class="form-control" name="reviewer-name" required>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="review-text" class="form-label">Your Review</label>
+                                    <textarea class="form-control" name="review-text" rows="3" required></textarea>
+                                </div>
+                                <div class="star-rating mb-3">
+                                    @for ($i = 1; $i <= 5; $i++)
                                         <span class="star" data-value="{{ $i }}">&#9733;</span>
-                                    @endif
-                                @endfor
-                            </div>
+                                    @endfor
+                                    <input type="hidden" name="rating" id="rating" value="">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit Review</button>
+                            </form>
                         </div>
                     </div>
-                @empty
-                    <p class="col-12">No reviews yet.</p>
-                @endforelse
+                </div>
+
+                <!-- Previous Reviews Row -->
+                <div class="row mt-3">
+        <div class="col-12">
+            <div class="reviews">
+                <div class="CustomerRev">
+                    <hr>
+                    <h3>Customer Reviews</h3>
+                    <hr>
+                </div>
+                <div class="row">
+                    @forelse ($product->reviews()->latest()->take(3)->get() as $review)
+                        <div class="col-md-4">
+                            <div class="review">
+                                <h4>{{ $review->reviewer_name }}</h4>
+                                <div class="review-text">
+                                    <p>{{ $review->review_text }}</p>
+                                </div>
+                                <div class="previous-star-rating">
+                                    @for ($i = 1; $i <= 5; $i++)
+                                        @if ($i <= $review->rating)
+                                            <span class="star filled" data-value="{{ $i }}">&#9733;</span>
+                                        @else
+                                            <span class="star" data-value="{{ $i }}">&#9733;</span>
+                                        @endif
+                                    @endfor
+                                </div>
+                            </div>
+                        </div>
+                    @empty
+                        <p class="col-12">No reviews yet.</p>
+                    @endforelse
+                </div>
             </div>
         </div>
     </div>
-</div>
+</head>
 
 <footer>
     @include('footer')
 </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-    <script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+<script>
     
-   
    document.addEventListener('DOMContentLoaded', function() {
     const stars = document.querySelectorAll('#review-form .star-rating .star');
 
